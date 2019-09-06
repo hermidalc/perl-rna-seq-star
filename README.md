a66322ebf597a7edf2c4d994505dc1a26cdf2ac7
Leandro Hermida <hermidal@cs.umd.edu>
2019-09-06 08:00:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,164 @@
+# STAR-HTSeq RNA-seq Pipeline Wrapper
+
+STAR-HTSeq RNA-seq processing pipeline environment and wrapper script,
+including SRA query, download, and caching functionality and useful
+reuse/restart features.
+
+## Installation
+
+Install latest
+<a href="https://docs.conda.io/en/latest/miniconda.html" target="_blank">
+Miniconda
+</a>
+or
+<a href="https://www.anaconda.com/distribution/" target="_blank">
+Anaconda
+</a>
+for Python 3.x. and make sure `conda` is updated.
+
+Clone the Github repository:
+
+```bash
+git clone git@github.com:hermidalc/rna-seq-star-htseq.git
+```
+
+Create `rna-seq-star-htseq` conda environment:
+
+```bash
+cd rna-seq-star-htseq
+conda env create -f environment.yml
+conda activate rna-seq-star-htseq
+```
+
+Configure SRA toolkit:
+
+The SRA toolkit prefetches files temporarily to the default cache directory
+which might not be where you want.  If so, either set the `CACHE`
+`public user-repository` location or under `TOOLS` prefetch
+downloads to the `current directory`:
+
+```bash
+vdb-config -i
+```
+
+Install Aspera Connect (optional):
+
+Downloading large .sra vdb files from SRA can sometimes result in repeated
+download failures when using `prefetch` with the default download method. If
+you install
+<a href="https://downloads.asperasoft.com/connect2/" target="_blank">
+Aspera Connect
+</a>
+then `prefetch` will automatically use it and I've found it results in more
+stable download performance.  To install Aspera Connect simply:
+
+```bash
+tar -xzf ibm-aspera-connect-3.9.6.173386-linux-g2.12-64.tar.gz
+./ibm-aspera-connect-3.9.6.173386-linux-g2.12-64.sh
+```
+
+No more configuration is necessary as `prefetch` will automatically find and
+use the `ascp` command.
+
+## Usage
+
+#### Creating a STAR genome index
+
+For example, for GRCh38 using the current NCI GDC FASTA and GENCODE GTF
+versions (which will require ~36GB RAM to map reads using this genome):
+
+```bash
+mkdir star_grch38p2_d1_vd1_gtfv22
+
+STAR \
+--runThreadN 12 \
+--runMode genomeGenerate \
+--genomeDir star_grch38p2_d1_vd1_gtfv22 \
+--genomeFastaFiles GRCh38.d1.vd1.fa \
+--sjdbGTFfile gencode.v22.annotation.gtf \
+--sjdbOverhang 100
+```
+
+#### Wrapper script usage
+
+```
+$ run_star_htseq.pl -h
+Usage:
+     run_star_htseq.pl [options]
+
+     Options:
+        --sra-query <str>    SRA query string to obtain SRR run metadata
+                             (required if no --srr-file)
+        --srr-file <file>    SRR ID list file
+                             (required if no --sra-query)
+        --out-dir <dir>      Output directory
+                             (default = current dir)
+        --tmp-dir <dir>      Temporary working directory
+                             (default = current dir)
+        --num-threads <n>    Number of parallel threads
+                             (default = num cpus)
+        --genome-dir <dir>   STAR genome index directory
+                             (default = star_grch38p2_d1_vd1_gtfv22)
+        --gtf-file <file>    Genome annotation GTF file
+                             (default = gencode.v22.annotation.gtf)
+        --genome-shm         Use STAR genome index in shared memory
+                             (default = false)
+        --refresh-meta       Re-query SRA to update metadata cache
+                             (default = false)
+        --query-only         Query SRA and then exit program
+                             (default = false)
+        --regen-all          Regenerate all result files
+                             (default = false)
+        --keep-all           Keep all result files
+                             (default = false)
+        --keep-bams          Keep STAR BAMs
+                             (default = false)
+        --gen-tx-bam         Generate STAR transcriptome-aligned BAM
+                             (default = false)
+        --no-htseq           Skip HTSeq quantification
+                             (default = false)
+        --htseq-mode         HTSeq --mode option
+                             (default = intersection-nonempty)
+        --htseq-stranded     HTSeq --stranded option
+                             (default = no)
+        --dry-run            Show what would've been done
+                             (default = false)
+        --verbose            Be verbose
+        --help               Display usage and exit
+        --version            Display program version and exit
+```
+
+The `--sra-query` can be any NCBI Entrez query string, e.g.
+
+```bash
+--sra-query 'SRP155030[All Fields] AND (1900[MDAT]:2900[MDAT] NOT "strategy exome"[Filter])'
+```
+
+#### Wrapper script features
+
+The `run_star_htseq.pl` wrapper script encapsulates the following steps:
+
+1.  Query SRA (or load SRR list) and then fetch and cache SRR metadata (with Entrez Direct tools)
+
+Then for each SRR:
+
+2.  Prefetch SRA vdb file (with SRA toolkit)
+3.  Validate SRA vdb file (with SRA toolkit)
+4.  Dump FASTQs from vdb (with parallel-fastq-dump wrapper to fastq-dump)
+5.  Run STAR alignment and gene expression quantification
+6.  Run HTSeq gene expression quantification
+
+The wrapper script also has some useful features, for example:
+
+*   It automatically caches downloaded SRA metadata for your query and will reuse cached metadata for the same SRA query.  Metadata can be refreshed with `--refresh-meta`.
+*   It automatically looks for and reuses any existing completed intermediate files from each processing step in case e.g. the script got killed. You simply restart the script with same parameters and it will come right back to where it left off. This can be overriden with `--regen-all`.
+*   If the processing fails for any SRR it will output that there was an error and automatically move on to the next.
+*   It saves a ton of space by not keeping intermediate result files after finishing processing for each SRR.  This can be overriden with the options `--keep-all` or for just BAMs `--keep-bams`.
+*   In my environment I've found HTSeq to be the slowest step in the pipeline by far, also because it is only single-threaded while the other compute intensive steps are parallelized.  So to greatly improve performance the wrapper script can run the HTSeq jobs in parallel at the end if you specify the option `--keep-bams`, though beware that this will take up a lot more space.
+
+## References
+
+<a href="https://www.ncbi.nlm.nih.gov/home/tools/" target="_blank">Entrez Direct utilities and SRA toolkit</a><br/>
+<a href="https://github.com/rvalieris/parallel-fastq-dump" target="_blank">parallel-fastq-dump</a><br/>
+<a href="https://github.com/alexdobin/STAR" target="_blank">STAR</a><br/>
+<a href="https://github.com/simon-anders/htseq" target="_blank">HTSeq</a>
---
+++ run_star_htseq.pl
@@ -0,0 +1,544 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+use sigtrap qw(handler sig_handler normal-signals error-signals ALRM);
+use Cwd qw(cwd);
+use Digest::MD5 qw(md5_hex);
+use File::Basename qw(fileparse);
+use File::Copy qw(move);
+use File::Path qw(make_path remove_tree);
+use File::Spec;
+use Getopt::Long qw(:config auto_help auto_version);
+use List::Util qw(max min notall uniq);
+use Parallel::ForkManager;
+use Pod::Usage qw(pod2usage);
+use POSIX qw(SIGINT);
+use Storable qw(lock_nstore lock_retrieve);
+use Term::ANSIColor;
+use Text::CSV qw(csv);
+use Unix::Processors;
+use Data::Dumper;
+
+sub sig_handler {
+    die "\nSTAR-HTSeq pipeline exited [", scalar localtime, "]\n";
+}
+
+our $VERSION = '0.1';
+
+$Data::Dumper::Sortkeys = 1;
+$Data::Dumper::Terse = 1;
+$Data::Dumper::Deepcopy = 1;
+
+# unbuffer error and output streams (make sure STDOUT
+# is last so that it remains the default filehandle)
+select(STDERR); $| = 1;
+select(STDOUT); $| = 1;
+
+# config
+use constant SRA_FASTQ_SUFFIX_FMT => '_pass_';
+my @bam_rg2sra_fields = (
+    { RG => 'ID', SRA => 'Run' },
+    { RG => 'SM', SRA => 'Sample' },
+    { RG => 'LB', SRA => 'LibraryName' },
+    { RG => 'PL', SRA => 'Platform' },
+    { RG => 'PM', SRA => 'Model' },
+);
+my $procs = Unix::Processors->new();
+
+# options
+my $sra_query_str = '';
+my $srr_file = '';
+my $out_dir = File::Spec->abs2rel();
+my $tmp_dir = cwd();
+my $num_threads = $procs->max_physical;
+my $genome_dir = 'star_grch38p2_d1_vd1_gtfv22';
+my $gtf_file = 'gencode.v22.annotation.gtf';
+my $genome_shm = 0;
+my $refresh_meta = 0;
+my $query_only = 0;
+my $regen_all = 0;
+my $keep_all = 0;
+my $keep_bams = 0;
+my $gen_tx_bam = 0;
+my $no_htseq = 0;
+my $htseq_mode = 'intersection-nonempty';
+my $htseq_stranded = 'no';
+my $dry_run = 0;
+my $verbose = 0;
+my $debug = 0;
+GetOptions(
+    'sra-query:s' => \$sra_query_str,
+    'srr-file:s' => \$srr_file,
+    'out-dir:s' => \$out_dir,
+    'tmp-dir:s' => \$tmp_dir,
+    'num-threads:i' => \$num_threads,
+    'genome-dir:s' => \$genome_dir,
+    'gtf-file:s' => \$gtf_file,
+    'genome-shm' => \$genome_shm,
+    'refresh-meta' => \$refresh_meta,
+    'query-only' => \$query_only,
+    'regen-all' => \$regen_all,
+    'keep-all' => \$keep_all,
+    'keep-bams' => \$keep_bams,
+    'gen-tx-bam' => \$gen_tx_bam,
+    'no-htseq' => \$no_htseq,
+    'htseq-mode' => \$htseq_mode,
+    'htseq-stranded' => \$htseq_stranded,
+    'dry-run' => \$dry_run,
+    'verbose' => \$verbose,
+    'debug' => \$debug,
+) || pod2usage(-verbose => 0);
+if (!$sra_query_str) {
+    if (!$srr_file) {
+        pod2usage(-message => 'Required --sra-query or --srr-file');
+    }
+    elsif (!-f $srr_file) {
+        pod2usage(-message => 'Invalid --srr-file');
+    }
+}
+if ($num_threads =~ /^\d+$/) {
+    $num_threads = min(max(0, $num_threads), $procs->max_physical);
+}
+else {
+    pod2usage(-message => '--num-threads must be an integer');
+}
+if (!$dry_run) {
+    make_path($out_dir) unless -d $out_dir;
+    make_path($tmp_dir) unless -d $tmp_dir;
+}
+$keep_bams = 0 if $keep_all;
+print "#", '-' x 120, "#\n",
+      "# STAR-HTSeq pipeline [" . scalar localtime() . "]\n\n";
+my $srr_metadata;
+if (!$sra_query_str) {
+    print "Reading $srr_file: ";
+    open(my $fh, '<', $srr_file);
+    chomp(my @srr_ids = <$fh>);
+    close($fh);
+    @srr_ids = grep { /\S/ && /^SRR/ } @srr_ids;
+    @srr_ids = sort { $a cmp $b } uniq @srr_ids;
+    print scalar(@srr_ids), " unique SRRs\n";
+    $sra_query_str = join(' OR ', map { "${_}[Accession]" } @srr_ids);
+}
+my $srr_metadata_pls_file =
+    "$tmp_dir/".md5_hex($sra_query_str).".srr_metadata.pls";
+if (!-f $srr_metadata_pls_file or $refresh_meta) {
+    $sra_query_str =~ s/'/\'/g;
+    my $sra_cmd_str = join(' | ',
+        "esearch -db sra -query '$sra_query_str'",
+        "efetch -format runinfo",
+    );
+    print 'Searching SRA:';
+    print +($verbose or $debug) ? "\n$sra_cmd_str\n" : ' ';
+    my $sra_meta_csv_str = `$sra_cmd_str`;
+    die +(-t STDERR ? colored('ERROR', 'red') : 'ERROR'),
+        ": efetch failed (exit code ", $? >> 8, ")\n" if $?;
+    $srr_metadata = csv(in => \$sra_meta_csv_str, headers => 'auto');
+    my %seen_srrs;
+    $srr_metadata = [ grep {
+        $_->{'Run'} =~ /\S/ &&
+        $_->{'Run'} =~ /^SRR/ &&
+        !$seen_srrs{$_->{'Run'}}++
+    } @{$srr_metadata} ];
+    $srr_metadata = [
+        sort { $a->{'Run'} cmp $b->{'Run'} } @{$srr_metadata}
+    ];
+    print scalar(@{$srr_metadata}), " runs found\n";
+    print "Caching query metadata\n";
+    if (!$dry_run) {
+        lock_nstore($srr_metadata, $srr_metadata_pls_file);
+    }
+}
+else {
+    print "Loading cached query metadata\n";
+    $srr_metadata = lock_retrieve($srr_metadata_pls_file);
+    print scalar(@{$srr_metadata}), " runs found\n";
+}
+if ($debug) {
+    print "SRR IDs: ", Dumper([ map { $_->{'Run'} } @{$srr_metadata} ]),
+          "SRR metadata: ", Dumper($srr_metadata);
+}
+print "\n";
+exit if $query_only;
+my @srrs_completed = ();
+my @htseq_cmd_data = ();
+for my $srr_meta (@{$srr_metadata}) {
+    my $srr_id = $srr_meta->{'Run'};
+    print "[$srr_id]\n";
+    my $srr_dir = File::Spec->abs2rel("$tmp_dir/$srr_id");
+    my @fastq_files = map {
+        "$srr_dir/${srr_id}".SRA_FASTQ_SUFFIX_FMT."$_.fastq.gz"
+    } 1..2;
+    my $star_bam_file_name = 'Aligned.out.bam';
+    my $star_bam_file = "$srr_dir/$star_bam_file_name";
+    my $star_tx_bam_file_name = 'Aligned.toTranscriptome.out.bam';
+    my $star_tx_bam_file = "$srr_dir/$star_tx_bam_file_name";
+    my $star_counts_file_name = 'ReadsPerGene.out.tab';
+    my $star_counts_file = "$srr_dir/$star_counts_file_name";
+    my $star_completed_file = "$srr_dir/star_completed";
+    my $final_star_bam_file_name = "${srr_id}.Aligned.bam";
+    my $final_star_bam_file = "$out_dir/$final_star_bam_file_name";
+    my $final_star_tx_bam_file_name = "${srr_id}.Aligned.toTranscriptome.bam";
+    my $final_star_tx_bam_file = "$out_dir/$final_star_tx_bam_file_name";
+    my $final_star_counts_file_name = "${srr_id}.star.counts.txt";
+    my $final_star_counts_file = "$out_dir/$final_star_counts_file_name";
+    my $htseq_counts_file_name = "${srr_id}.htseq.counts.txt";
+    my $htseq_counts_file =
+        ($keep_all ? $srr_dir : $out_dir)."/$htseq_counts_file_name";
+    my $ran_star = 0;
+    if (
+        $regen_all or (
+            (
+                $keep_bams and (
+                    (!-f $final_star_bam_file and !-f $star_bam_file) or
+                    (
+                        $gen_tx_bam and
+                        !-f $final_star_tx_bam_file and !-f $star_tx_bam_file
+                    )
+                )
+            ) or
+            (!-f $final_star_counts_file and !-f $star_counts_file) or
+            (-d $srr_dir and !-f $star_completed_file)
+        )
+    ) {
+        if (!$dry_run) {
+            if (-d $srr_dir) {
+                remove_tree($srr_dir, { safe => 1 }) if $regen_all;
+            }
+            else {
+                make_path($srr_dir);
+            }
+        }
+        if ($regen_all or !-f "$srr_dir/${srr_id}.sra") {
+            my $pf_cmd_str = "prefetch $srr_id -o '$srr_dir/${srr_id}.sra'";
+            print "Prefetching ${srr_id}.sra";
+            print "\n$pf_cmd_str" if $verbose or $debug;
+            if (!$dry_run) {
+                if (system($pf_cmd_str)) {
+                    exit(SIGINT) if ($? & 127) == SIGINT;
+                    warn +(-t STDERR ? colored('ERROR', 'red') : 'ERROR'),
+                        ": prefetch failed (exit code ", $? >> 8, ")\n\n";
+                    next;
+                }
+            }
+            else {
+                print "\n";
+            }
+        }
+        else {
+            print "Using existing ${srr_id}.sra\n";
+        }
+        if ($regen_all or notall { -f } @fastq_files) {
+            my $val_cmd_str = join(' ', (
+                "vdb-validate",
+                "-x", "-B yes", "-I yes", "-C yes",
+                "'$srr_dir/${srr_id}.sra'",
+            ));
+            print "Validating ${srr_id}.sra\n";
+            print "$val_cmd_str\n" if $verbose or $debug;
+            if (!$dry_run) {
+                if (system($val_cmd_str)) {
+                    exit(SIGINT) if ($? & 127) == SIGINT;
+                    warn +(-t STDERR ? colored('ERROR', 'red') : 'ERROR'),
+                        ": vdb-validate failed (exit code ", $? >> 8, ")\n\n";
+                    next;
+                }
+            }
+            my $pfq_cmd_str = join(' ', (
+                "parallel-fastq-dump",
+                "--threads $num_threads",
+                "--sra-id '$srr_dir/${srr_id}.sra'",
+                "--outdir '$srr_dir'",
+                "--tmpdir '$srr_dir'",
+                "--gzip",
+                "--skip-technical",
+                "--readids",
+                "--read-filter pass",
+                "--dumpbase",
+                "--split-3",
+                "--clip",
+            ));
+            print "Generating ${srr_id}.sra FASTQs\n";
+            print "$pfq_cmd_str\n" if $verbose or $debug;
+            if (!$dry_run) {
+                if (system($pfq_cmd_str)) {
+                    exit(SIGINT) if ($? & 127) == SIGINT;
+                    warn +(-t STDERR ? colored('ERROR', 'red') : 'ERROR'),
+                        ": parallel-fastq-dump failed (exit code ", $? >> 8,
+                        ")\n\n";
+                    next;
+                }
+            }
+        }
+        else {
+            print "Using existing ",
+                  join(' ', map { (fileparse($_))[0] } @fastq_files), "\n";
+        }
+        my @bam_rg_fields = map {
+            "\"$_->{'RG'}:$srr_meta->{$_->{'SRA'}}\""
+        } grep {
+            $srr_meta->{$_->{'SRA'}} =~ /\S/
+        } @bam_rg2sra_fields;
+        my @quant_modes = ('GeneCounts');
+        push @quant_modes, 'TranscriptomeSAM' if $gen_tx_bam;
+        my $star_cmd_str = join(' ', (
+            "STAR",
+            "--runThreadN $num_threads",
+            "--readFilesIn", join(' ', map { "'$_'" } @fastq_files),
+            "--alignIntronMax 1000000",
+            "--alignIntronMin 20",
+            "--alignMatesGapMax 1000000",
+            "--alignSJDBoverhangMin 1",
+            "--alignSJoverhangMin 8",
+            "--alignSoftClipAtReferenceEnds Yes",
+            "--chimJunctionOverhangMin 15",
+            "--chimMainSegmentMultNmax 1",
+            "--chimOutType Junctions SeparateSAMold WithinBAM SoftClip",
+            "--chimSegmentMin 15",
+            "--genomeDir '$genome_dir'",
+            "--genomeLoad", $genome_shm ? "LoadAndKeep" : "NoSharedMemory",
+            "--limitSjdbInsertNsj 1200000",
+            "--outFileNamePrefix '$srr_dir/'",
+            "--outSAMattrRGline", join(' ', @bam_rg_fields),
+            "--outFilterIntronMotifs None",
+            "--outFilterMatchNminOverLread 0.33",
+            "--outFilterMismatchNmax 999",
+            "--outFilterMismatchNoverLmax 0.1",
+            "--outFilterMultimapNmax 20",
+            "--outFilterScoreMinOverLread 0.33",
+            "--outFilterType BySJout",
+            "--outSAMattributes NH HI AS nM NM ch",
+            "--outSAMstrandField intronMotif",
+            "--outSAMtype BAM Unsorted",
+            "--outSAMunmapped Within",
+            "--quantMode", join(' ', @quant_modes),
+            "--readFilesCommand zcat",
+            "--twopassMode", $genome_shm ? "None" : "Basic",
+        ));
+        print "Running STAR alignment\n";
+        print "$star_cmd_str\n" if $verbose or $debug;
+        if (!$dry_run) {
+            if (system($star_cmd_str)) {
+                exit(SIGINT) if ($? & 127) == SIGINT;
+                warn +(-t STDERR ? colored('ERROR', 'red') : 'ERROR'),
+                    ": STAR failed (exit code ", $? >> 8, ")\n\n";
+                next;
+            }
+            open(my $fh, '>', $star_completed_file);
+        }
+        $ran_star++;
+    }
+    if ($keep_bams) {
+        if (
+            $regen_all or !-f $final_star_bam_file or (
+                -f $star_bam_file and
+                (stat($final_star_bam_file))[7] !=
+                (stat($star_bam_file))[7]
+            )
+        ) {
+            if (!$ran_star) {
+                print "Using existing $star_bam_file_name\n";
+            }
+            print "Moving $star_bam_file_name",
+                  ' -> ',
+                  "$final_star_bam_file_name\n";
+            if (!$dry_run) {
+                move($star_bam_file, $final_star_bam_file);
+            }
+        }
+        else {
+            print "Completed $final_star_bam_file_name exists\n";
+        }
+        if ($gen_tx_bam) {
+            if (
+                $regen_all or !-f $final_star_tx_bam_file or (
+                    -f $star_tx_bam_file and
+                    (stat($final_star_tx_bam_file))[7] !=
+                    (stat($star_tx_bam_file))[7]
+                )
+            ) {
+                if (!$ran_star) {
+                    print "Using existing $star_tx_bam_file_name\n";
+                }
+                print "Moving $star_tx_bam_file_name",
+                      ' -> ',
+                      "$final_star_tx_bam_file_name\n";
+                if (!$dry_run) {
+                    move($star_tx_bam_file, $final_star_tx_bam_file);
+                }
+            }
+            else {
+                print "Completed $final_star_tx_bam_file_name exists\n";
+            }
+        }
+    }
+    if (
+        $regen_all or !-f $final_star_counts_file or (
+            -f $star_counts_file and
+            (stat($final_star_counts_file))[7] !=
+            (stat($star_counts_file))[7]
+        )
+    ) {
+        if (!$ran_star) {
+            print "Using existing $star_counts_file_name\n";
+        }
+        print "Moving $star_counts_file_name",
+              ' -> ',
+              "$final_star_counts_file_name\n";
+        if (!$dry_run) {
+            move($star_counts_file, $final_star_counts_file);
+        }
+    }
+    else {
+        print "Completed $final_star_counts_file_name exists\n";
+    }
+    if (!$no_htseq) {
+        if (
+            $regen_all or !-f $htseq_counts_file or
+            (stat($htseq_counts_file))[7] == 0
+        ) {
+            if (!-f $star_bam_file) {
+                $star_bam_file_name = $final_star_bam_file_name;
+                $star_bam_file = $final_star_bam_file;
+            }
+            my $htseq_cmd_str = join(' ', (
+                "htseq-count",
+                "--format bam",
+                "--order name",
+                "--stranded $htseq_stranded",
+                "--minaqual 10",
+                "--type exon",
+                "--idattr gene_id",
+                "--mode $htseq_mode",
+                $debug ? "" : "--quiet",
+                "'$star_bam_file'",
+                "'$gtf_file'",
+            ));
+            $htseq_cmd_str =~ s/\s+/ /g;
+            if (!$keep_bams) {
+                if (!$ran_star) {
+                    print "Using existing $star_bam_file_name\n";
+                }
+                print "Running HTSeq quantification\n";
+                print "$htseq_cmd_str\n" if $verbose or $debug;
+                if (!$dry_run) {
+                    open(my $fh, '>', $htseq_counts_file);
+                    my $htseq_counts = `$htseq_cmd_str`;
+                    if ($?) {
+                        exit(SIGINT) if ($? & 127) == SIGINT;
+                        warn +(-t STDERR ? colored('ERROR', 'red') : 'ERROR'),
+                            ": htseq-count failed (exit code ", $? >> 8,
+                            ")\n\n";
+                        next;
+                    }
+                    else {
+                        print $fh $htseq_counts;
+                    }
+                    close($fh);
+                }
+            }
+            else {
+                push @htseq_cmd_data, {
+                    'srr_id' => $srr_id,
+                    'cmd_str' => $htseq_cmd_str,
+                    'counts_file' => $htseq_counts_file,
+                }
+            }
+        }
+        else {
+            print "Completed $htseq_counts_file_name exists\n";
+        }
+    }
+    if (!$dry_run and !$keep_all and -d $srr_dir) {
+        remove_tree($srr_dir, { safe => 1 });
+    }
+    push @srrs_completed, $srr_id;
+    print "\n";
+}
+if (@htseq_cmd_data) {
+    print "Running HTSeq quantification\n";
+    my $pm = Parallel::ForkManager->new($num_threads);
+    for my $htseq_cmd (@htseq_cmd_data) {
+        $pm->start and next;
+        print "[$htseq_cmd->{'srr_id'}] Starting htseq-count\n";
+        print "[$htseq_cmd->{'srr_id'}] $htseq_cmd->{'cmd_str'}\n"
+            if $verbose or $debug;
+        if (!$dry_run) {
+            open(my $fh, '>', $htseq_cmd->{'counts_file'});
+            my $htseq_counts = `$htseq_cmd->{'cmd_str'}`;
+            if ($?) {
+                exit(SIGINT) if ($? & 127) == SIGINT;
+                warn "[$htseq_cmd->{'srr_id'}] ", (
+                        -t STDERR ? colored('ERROR', 'red') : 'ERROR'
+                     ), ": htseq-count failed (exit code ", $? >> 8, ")\n";
+                next;
+            }
+            else {
+                print $fh $htseq_counts;
+            }
+            close($fh);
+        }
+        print "[$htseq_cmd->{'srr_id'}] Finished htseq-count\n";
+        $pm->finish;
+    }
+    $pm->wait_all_children;
+    print "\n";
+}
+print scalar(@srrs_completed), " / ", scalar(@{$srr_metadata}),
+      " SRRs completed\n\n",
+      "STAR-HTSeq pipeline complete [", scalar localtime, "]\n\n";
+
+__END__
+
+=head1 NAME
+
+run_star_htseq.pl - Run STAR-HTSeq Pipeline
+
+=head1 SYNOPSIS
+
+ run_star_htseq.pl [options]
+
+ Options:
+    --sra-query <str>    SRA query string to obtain SRR run metadata
+                         (required if no --srr-file)
+    --srr-file <file>    SRR ID list file
+                         (required if no --sra-query)
+    --out-dir <dir>      Output directory
+                         (default = current dir)
+    --tmp-dir <dir>      Temporary working directory
+                         (default = current dir)
+    --num-threads <n>    Number of parallel threads
+                         (default = num cpus)
+    --genome-dir <dir>   STAR genome index directory
+                         (default = star_grch38p2_d1_vd1_gtfv22)
+    --gtf-file <file>    Genome annotation GTF file
+                         (default = gencode.v22.annotation.gtf)
+    --genome-shm         Use STAR genome index in shared memory
+                         (default = false)
+    --refresh-meta       Re-query SRA to update metadata cache
+                         (default = false)
+    --query-only         Query SRA and then exit program
+                         (default = false)
+    --regen-all          Regenerate all result files
+                         (default = false)
+    --keep-all           Keep all result files
+                         (default = false)
+    --keep-bams          Keep STAR BAMs
+                         (default = false)
+    --gen-tx-bam         Generate STAR transcriptome-aligned BAM
+                         (default = false)
+    --no-htseq           Skip HTSeq quantification
+                         (default = false)
+    --htseq-mode         HTSeq --mode option
+                         (default = intersection-nonempty)
+    --htseq-stranded     HTSeq --stranded option
+                         (default = no)
+    --dry-run            Show what would've been done
+                         (default = false)
+    --verbose            Be verbose
+    --help               Display usage and exit
+    --version            Display program version and exit
+
+=cut
